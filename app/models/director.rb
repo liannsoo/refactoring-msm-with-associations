411f770d8e14904eff3cae330fe8e52a39d5e4dc
liannsoo <asoosai@uchicago.edu>
2024-11-13 00:18:45 +0000

    2

--- app/models/director.rb
+++ app/models/director.rb
@@ -12,6 +12,6 @@
 #
 class Director < ApplicationRecord
   validates(:name, presence: true)
-  has_many(:filmography, class_name: "Movie", foreign_key: "director_id")
+  has_many(:filmography, :class_name => "Movie", :foreign_key => "director_id")
 
 end
